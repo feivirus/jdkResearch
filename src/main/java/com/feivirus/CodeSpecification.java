91c14c7db99fa1b17995d119ab85cb75002c542f
jingtian <jifei.zheng@ideacome.com>
2018-07-15 19:31:45 +0800

    添加常用代码命名规范

---
+++ src/main/java/com/feivirus/CodeSpecification.java
@@ -0,0 +1,20 @@
+package com.feivirus;
+
+/**
+ * 
+ * @author feivirus
+ *
+ *常用命名后缀
+事件 event-handler,listener，actor,reactor,task
+请求/报文 request-context-connector-parse-processor-response
+公共库/三方库 manager
+流/网络/管道 input-output,filter,parser
+对象封装 holder,container,keeper,wrapper,detector,creator,generator,inspector,monitor,operator,sponsor,worker,reader,collector,editor,
+guard,executor,xx pool,xxx,convertor util,helper,binder,
+接口 impl,api
+分层 entity,dao
+设计模式 factory,chain,builder,abstract,facade,observer,adaptor,decorator,redirector,command,successor,visitor,producer,instructor
+ */
+public class CodeSpecification {
+
+}
