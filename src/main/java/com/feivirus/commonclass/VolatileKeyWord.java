87757e236c3a02cbf8ab46c6dcd2c9e90914dd3e
jingtian <jifei.zheng@ideacome.com>
2018-10-17 14:26:11 +0800

    volatile关键字

---
+++ src/main/java/com/feivirus/commonclass/VolatileKeyWord.java
@@ -0,0 +1,41 @@
+package com.feivirus.commonclass;
+
+/**
+ * 
+ * @author feivirus
+ *
+ */
+public class VolatileKeyWord {
+	public static void main(String[] args) throws InterruptedException {
+		PrintString printString = new PrintString();
+		Thread thread = new Thread(printString);
+		thread.start();
+		Thread.sleep(1000);
+		printString.setRunLoop(false);
+		System.out.println("主线程终止");
+	}
+}
+
+class PrintString implements Runnable{
+	volatile private boolean isRunLoop = true;
+
+	public boolean isRunLoop() {
+		return isRunLoop;
+	}
+
+	public void setRunLoop(boolean isRunLoop) {
+		this.isRunLoop = isRunLoop;
+	}
+
+	public void run() {
+		try {
+			while (isRunLoop) {
+				//System.out.println("I am running, " + Thread.currentThread().getName());
+				//Thread.sleep(1000);
+			}
+			System.out.println("子线程被终止");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}		
+}
