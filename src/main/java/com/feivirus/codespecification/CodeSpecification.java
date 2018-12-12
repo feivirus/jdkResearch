ca19f1ed9c468e34b0edbe5108a3f8a5acb28845
jingtian <jifei.zheng@ideacome.com>
2018-12-12 14:44:39 +0800

    提交状态机说明

--- src/main/java/com/feivirus/codespecification/CodeSpecification.java
+++ src/main/java/com/feivirus/codespecification/CodeSpecification.java
@@ -9,11 +9,13 @@ package com.feivirus.codespecification;
 请求/报文 request-context-connector-parse-processor-response
 公共库/三方库 manager
 流/网络/管道 input-output,filter,parser
-对象封装 holder,container,keeper,wrapper,detector,creator,generator,inspector,monitor,operator,sponsor,worker,reader,collector,editor,
-guard,executor,xx pool,xxx,convertor util,helper,binder,
+对象封装 holder,container,keeper,wrapper,detector,creator,generator,inspector,
+monitor,operator,sponsor,worker,reader,collector,editor,
+guard,executor,xx pool,xxx convertor util,helper,binder,xxx parser
 接口 impl,api
 分层 entity,dao
-设计模式 factory,chain,builder,abstract,facade,observer,adaptor,decorator,redirector,command,successor,visitor,producer,instructor
+设计模式 factory,chain,builder,abstract,facade,observer,adaptor,decorator,redirector,
+command,successor,visitor,producer,instructor
  */
 public class CodeSpecification {
 	/***
@@ -22,7 +24,7 @@ public class CodeSpecification {
 	 * 1.类，变量，方法命名尽量简短，比如readAndCheckByRow()，考虑readRow().最好一两个单词，不超过三个单词
 	 * 2.能用接口实现的不用类
 	 * 3.能用引用的不用继承
-	 * 4.函数参数多，超过三个用大对象DTO
+	 * 4.函数参数多，超过三个用大对象DTO，包括controller里面和前端传递的查询参数
 	 * 5.和前端传递参数能用Spring的@Validate校验的不写冗余代码校验
 	 * 6.日志打印用{}，不用+
 	 * 7.错误信息用枚举定义，不用普通字串打印
@@ -35,6 +37,22 @@ public class CodeSpecification {
 	 * 14.工厂+策略是最常用设计模式的组合，能提出公用接口的，不写具体类.
 	 * 15.表，或者列，已有相同业务定义的，不重复添加
 	 * 16.能用mongo的尽量不用mysql存储json.
+	 * 17.service的增、删、改一定要有事务
+	 * 18.Mybatis的sql语句里面不用map，只用xxxDTO，map看不出来参数具体是什么
+	 * 19.业务代码里面不包含main方法，统一写junit单元测试
+	 * 20.接口和实现类的命名有两套规则,实现类和接口不一定, 也不建议要采用 Service -> ServiceImpl的对应方式, 
+	 * 因为这样做会让设计的局限性很大, 大家可以参考一下Spring里面的命名方式
+		BeanDefinition -> RootBeanDefinition
+		BeanDefinition -> GenericBeanDefinition
+		//这种命名就非常形象,容易理解
+	 * 21.业务抛出异常抛出RuntimeException类的子类
+	 * 22.mysql建表必备字段 d, create_time, creator, modify_time, modifier, deleted_id(或者sys_flag)
+	 * 23.超过三个表禁止 join。需要 join 的字段，数据类型必须绝对一致;多表关联查询时， 保证被关联的字段需要有索引
+	 * 24.mybatis所有的更新, 必须使用 MyBatis里面的 Mapper, updateXXXXSelective
+	 * 25.比如导入结果ImportResult记录，用ImportResult还是用ImportResultDTO
+	 * TODO
+	 * 26.?变量或者类命名用AbstractConvertProcessor动词 + 名词形式，还是AbstractConvertorProcessor
+	 * 	  名词 + 名词形式,待定
 	 */
  
 }
---
+++ src/main/java/com/feivirus/statemachine/StateMachine.java
@@ -0,0 +1,18 @@
+package com.feivirus.statemachine;
+
+/**
+ * 
+ * 自动机
+ * @author feivirus
+ * 应用:比如订单状态切换
+ * 参考
+ * 1.https://www.jianshu.com/p/326bd3ac2bf2?winzoom=1
+ * 2.http://www.cnblogs.com/bastard/archive/2012/06/05/2536258.html
+ * 3.https://www.jianshu.com/p/f3ad78613072
+ * 4.http://smc.sourceforge.net/
+ * 5.http://unimod.sourceforge.net/fsm-framework.html
+ * 6.http://www.blogjava.net/caizh2009/archive/2010/07/26/327157.html
+ */
+public class StateMachine {
+
+}
