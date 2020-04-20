31f8aa76caf9566180f34ba33fa50fcb186dd46d
jingtian <jifei.zheng@ideacome.com>
2020-04-20 11:20:50 +0800

    添加二进制字符串交换题目

---
+++ src/main/java/com/feivirus/algorithm/linklist/LRUCache.java
@@ -0,0 +1,8 @@
+package com.feivirus.algorithm.linklist;
+
+/**
+ * @author feivirus
+ * 设计一个带有过期时间的LRU缓存
+ */
+public class LRUCache {
+}
--- src/main/java/com/feivirus/algorithm/string/SortBinaryString.java
+++ src/main/java/com/feivirus/algorithm/string/SortBinaryString.java
@@ -6,4 +6,36 @@ package com.feivirus.algorithm.string;
  * 对由010101等组成的字串排序，排序成000111
  */
 public class SortBinaryString {
+
+    public static void main(String[] args) {
+        SortBinaryString sortBinaryString = new SortBinaryString();
+        String result = sortBinaryString.sort("010101");
+        System.out.println("result: " + result);
+    }
+
+    public String sort(String binaryString) {
+        int length = binaryString.length();
+        int start = 0, end = length - 1;
+        char startChar, endChar;
+        StringBuffer result = new StringBuffer(binaryString);
+
+        while (start < end) {
+            do {
+                startChar = (char)binaryString.charAt(start);
+                start++;
+            } while (startChar == '0' && start < end);
+
+            do {
+                endChar = (char)binaryString.charAt(end);
+                end--;
+            } while (endChar == '1' && start < end);
+
+            if (start < end) {
+                result.setCharAt(start - 1, '0');
+                result.setCharAt(end + 1, '1');
+            }
+        }
+
+        return result.toString();
+    }
 }
